0625358db2ca60c1423719e0ff74cc3e5856db07
CJ3032221 <CJ3032221@LAB01-PC04.ifspcjoad.ifspcjo.edu.br>
2026-06-17 20:28:18 -0300

    Criação da primeira requisição Get para listar todos os exercicios

--- siem/src/main/java/br/com/devpaulo/siem/controller/ExerciseController.java
+++ siem/src/main/java/br/com/devpaulo/siem/controller/ExerciseController.java
@@ -1,5 +1,29 @@
 package br.com.devpaulo.siem.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.devpaulo.siem.domain.model.Exercise;
+import br.com.devpaulo.siem.repository.ExerciseRepository;
+import br.com.devpaulo.siem.service.ExerciseService;
+
+@RestController
+@RequestMapping("/exercise")
 public class ExerciseController {
 
+	@Autowired
+	private ExerciseRepository exerciseRepository;
+	
+	@Autowired
+	private ExerciseService exerciseService;
+	
+	@GetMapping
+	public List<Exercise> findAll(){
+		return exerciseRepository.findAll();
+	}
+	
 }
