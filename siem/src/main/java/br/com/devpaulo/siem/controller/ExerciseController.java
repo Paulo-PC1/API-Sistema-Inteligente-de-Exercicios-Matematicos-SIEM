bedcd39a5b26a188163597799ff8145cbc275970
paulo <paulocezarleite91@gmail.com>
2026-06-23 19:04:33 -0300

    Finalização dos Métodos Http da entidade Exercise

--- siem/src/main/java/br/com/devpaulo/siem/controller/ExerciseController.java
+++ siem/src/main/java/br/com/devpaulo/siem/controller/ExerciseController.java
@@ -1,15 +1,26 @@
 package br.com.devpaulo.siem.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.devpaulo.siem.domain.model.Exercise;
 import br.com.devpaulo.siem.repository.ExerciseRepository;
 import br.com.devpaulo.siem.service.ExerciseService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/exercise")
@@ -26,4 +37,36 @@ public class ExerciseController {
 		return exerciseRepository.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Exercise> findById(@PathVariable Long id){
+		Optional<Exercise> exercise = exerciseRepository.findById(id);
+		if(exercise.isPresent()) {
+			return ResponseEntity.ok(exercise.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Exercise createExercise(@Valid @RequestBody Exercise exercise, HttpServletResponse response) {
+		return exerciseRepository.save(exercise);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removeExersice(@PathVariable Long id) {
+		exerciseRepository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Exercise> updateExercise(@PathVariable Long id, @Valid @RequestBody Exercise exercise) {
+		Exercise exerciseSaved = exerciseService.update(id, exercise);
+		return ResponseEntity.ok(exerciseSaved);
+	}
+
+	@PutMapping("/{id}/active")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void updateActiveProperty(@PathVariable Long id, @RequestBody Boolean active) {
+		exerciseService.updateActiveProperty(id, active);
+	}
 }
--- siem/src/main/java/br/com/devpaulo/siem/service/ExerciseService.java
+++ siem/src/main/java/br/com/devpaulo/siem/service/ExerciseService.java
@@ -1,5 +1,32 @@
 package br.com.devpaulo.siem.service;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import br.com.devpaulo.siem.domain.model.Exercise;
+import br.com.devpaulo.siem.repository.ExerciseRepository;
+
 public class ExerciseService {
 
+	@Autowired
+	public ExerciseRepository exerciseRepository;
+
+	public Exercise update(Long id, Exercise exercise) {
+		Exercise exerciseSaved = findUserById(id);
+		BeanUtils.copyProperties(exercise, exerciseSaved, "id");
+		return exerciseRepository.save(exerciseSaved);
+	}
+
+	public void updateActiveProperty(Long id, Boolean active) {
+		Exercise userSaved = findUserById(id);
+		userSaved.setActive(active);
+		exerciseRepository.save(userSaved);
+	}
+
+	public Exercise findUserById(Long id) {
+		Exercise exerciseSaved = exerciseRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return exerciseSaved;
+	}
+	
 }
