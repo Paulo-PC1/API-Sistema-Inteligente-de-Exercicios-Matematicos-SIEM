33f15342ca5ec33132ba04dfcd1ef5a5b10b799e
Paulo Cezar Leite dos Santos Junior <103606442+Paulo-PC1@users.noreply.github.com>
2026-06-30 20:05:47 -0300

    Update Exercise.java

--- siem/src/main/java/br/com/devpaulo/siem/domain/model/Exercise.java
+++ siem/src/main/java/br/com/devpaulo/siem/domain/model/Exercise.java
@@ -24,7 +24,6 @@ public class Exercise {
 	@NotBlank
 	private String statement;
 	private String operation;
-	@NotBlank
 	@NotNull
 	@Column(name = "correct_answer")
 	private Double correctAnswer;
